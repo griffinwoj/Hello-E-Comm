3bf4f8b4affe48c1becf2f88eb210bb29746b502
griffinw@protonmail.com <griffinw@protonmail.com>
2023-05-10 10:16:43 -0400

    adjusted seed function

--- seeds/index.js
+++ seeds/index.js
@@ -6,7 +6,7 @@ const seedProductTags = require('./product-tag-seeds');
 // Require the sequelize instance from the config/connection module
 const sequelize = require('../config/connection');
 // Define a function to seed all data and sync the database
-const seedAll = async () => {
+const seed = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedCategories();
@@ -24,4 +24,5 @@ const seedAll = async () => {
   process.exit(0);
 };
 // Call the seedAll function to seed the database.
-seedAll();
+//documentation for seedAll function at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+seed();
